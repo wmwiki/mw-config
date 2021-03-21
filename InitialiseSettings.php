5fec97be21f0c9e7416984e0a6157c1c5f6c623f
syma51 <78367257+syma51@users.noreply.github.com>
2021-03-21 14:49:37 +0700

    Create InitialiseSettings.php

---
+++ InitialiseSettings.php
@@ -0,0 +1,148 @@
+<?php
+include('/data/project/wm/priv.php');
+$wgConf->settings = [
+  'wgServer' => [
+    $wgDBmainwiki => 'https://wm.toolforge.org',
+  ],
+  'wgCanonicalServer' => [
+    $wgDBmainwiki => 'https://wm.toolforge.org',
+  ],
+  'wgScriptPath' => [
+    'default' => '',
+  ],
+//   'wgArticlePath' => [
+//    'default' => '/index.php?title=$1',
+//   ],
+  'wgLanguageCode' => [
+    $wgDBmainwiki => 'en',
+  ],
+  'wgLocalInterwiki' => [
+    $wgDBmainwiki => 'en',
+  ],
+  'wgAddGroups' => [
+    'default' => [
+      'bureaucract' => [ 'sysop', 'bureaucrat' ],
+    ],
+  ],
+// CentralAuth // !becarefull
+
+'wgCentralAuthDatabase' => [
+    'default' => $wgDBcentralauth,
+],
+
+'wgCentralAuthAutoNew' => [
+    'default' => true,
+],
+
+'wgCentralAuthAutoMigrate' => [
+    'default' => true,
+],
+
+'wgCentralAuthAutoMigrateNonGlobalAccounts' => [
+    'default' => true,
+],
+
+'wgCentralAuthCookies' => [
+    'default' => true,
+],
+
+// 'wgCentralAuthCookieDomain' => [
+    // 'default' => '.toolforge.org'
+// ],
+
+// 'wgCentralAuthAutoLoginWikis' => [
+// ],
+
+'wgCentralAuthCreateOnView' => [
+    'default' =>  true,
+],
+
+'wgCentralAuthEnableGlobalRenameRequest' => [
+    'default' =>  true,
+],
+
+'wgCentralAuthEnableUserMerge' => [
+    'default' =>  true,
+],
+
+// 'wgCentralAuthLoginWiki' => [
+    // 'default' =>  '',
+// ],
+
+'wgCentralAuthLoginIcon' => [
+    'default' => true,
+],
+
+'wgCentralAuthSilentLogin' => [
+    'default' =>  true,
+],
+
+'wgGlobalRenameBlacklistRegex' => [
+	'default' => true,
+],
+
+'wgGlobalRenameBlacklist' => [
+	'default' => 'https://meta.wikimedia.org/w/index.php?title=Global_rename_blacklist&action=raw'
+],
+
+// CentralNotice
+'wgNoticeInfrastructure' => [
+	'default' => false,
+	$wgDBmainwiki => true,
+],
+'wgCentralSelectedBannerDispatcher' => [
+	'default' => "https://wm.toolforge.org/index.php/Special:BannerLoader",
+],
+'wgCentralBannerRecorder' => [
+	'default' => "https://wm.toolforge.org/index.php/Special:RecordImpression",
+],
+'wgCentralDBname' => [
+	'default' => $wgDBmainwiki,
+],
+'wgCentralHost' => [
+	'default' => "https://wm.toolforge.org",
+],
+'wgNoticeProject' => [
+	'default' => 'all',
+	$wgDBmainwiki => 'all',
+],
+'wgNoticeProjects' => [
+	'default' => [
+		'all',
+		'optout',
+	],
+],
+'wgNoticeCookieDomain' => [
+    'default' => '',
+],
+'wgNoticeUseTranslateExtension' => [
+	'default' => false,
+],
+// Checkuser
+'wgCheckUserGBtoollink' => [
+	'centralDB' => $wgDBmainwiki,
+	'groups' => [ 'steward', 'staff' ]
+],
+
+'wgCheckUserForceSummary' => [
+    'default' => true,
+],
+
+'wgCheckUserCIDRLimit' => [ 
+    'IPv4' => 16,
+    'IPv6' => 32
+],
+
+'wgCheckUserCAMultiLock' => [
+    'centralDB' => $wgDBmainwiki,
+    'groups' => [ 'steward' ]
+],
+
+'wgCheckUserCAtoollink ' => [ 
+    'default' => $wgDBmainwiki,
+],
+
+'wgCheckUserEnableSpecialInvestigate' => [
+    'default' => true,
+],
+];
